ddd97b8067a8260ad427f14e605156320bbab104
Minh Pham <mqpham848@gmail.com>
2019-07-16 23:25:30 -0400

    Final

--- assets/main.js
+++ assets/main.js
@@ -3,102 +3,92 @@
 
 $(document).ready(function() {
 
-// array that holds buttons
-var cars = ['BMW', 'Mercedes', 'Audi', 'Lexus', 'Ferrari', 'Lamborghini'];
+    // array that holds buttons
+    let cars = ['BMW', 'Mercedes', 'Audi', 'Lexus', 'Ferrari', 'Lamborghini'];
 
+    //function that will make the buttons and add them to page 
+    function populateButtons(arrayToUse, classToAdd, areaToAddTo) {
+        $(areaToAddTo).empty();
+    
+        // looping through the array of movies
+        for (let i = 0; i < arrayToUse.length; i++){
 
-// function that makes button and adds them to the page
-function renderButtons() {
-
-    // erases everything in the div so there aren't duplicates
-    $("#button-View").empty();
-
-    // looping through the array of movies
-    for (let i = 0; i < cars.length; i++){
-
-        // dynamically generating buttons for each movie in the array.
-         let a = $("<button>");
-         //adding a class
-         a.addClass("car");
-        // Adding a data-attribute with a value of the cars at index i
-        a.attr("data-name", cars[i]);
-        // Providing the button's text with a value of the movie at index i
-          a.text(cars[i]);
-        // Adding the button to the HTML
-         $("#button-View").append(a);
+            let a = $("<button>");
+            a.addClass(classToAdd);
+            a.attr("data-type", arrayToUse[i]);
+            a.text(arrayToUse[i]);
+            $(areaToAddTo).append(a);
         }
     }
 
-// create a function that will populate images from Giphy API
-$(document).on("click", ".car-button", function(){
-    $("#images").empty();
+    // create function that will populate images from giphy API
+    $(document).onabort("click",".car-button", function(){
+        $("#images").empty();
 
-    $(".car-button").removeClass("active");
-    $(this).addClass("active");
+        $(".car-button").removeClass("active");
+        $(this).addClass("active");
 
-    let type = $(this).attr("data-type");
-    let queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    car + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+        let car = $(this).attr("data-type");
+        let queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        car + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+    });
 
-    //ajax call
-
+    // ajax call
     $.ajax({
-        url:queryURL,
+        url: queryURL,
         method: "GET"
     })
-
-    .then(function(response){
+    
+    .then(function(response) {
         let results = response.data;
-
-        for(var i = 0; i < results.length; i++){
+        
+        for (let i = 0; i < results.length; i++){
             let carDiv = $("<div class=\"car-item\">");
 
-            let rating = results[i].rating;
+            let rating =results[i].rating;
 
             let p = $("<p>").text("Rating: " + rating);
 
-            let animation = results[i].images.fixed_height.url;
+            let animate = results[i].images.fixed_height.url;
             let still = results[i].images.fixed_height_still.url;
 
             let carImages = $("<img>");
             carImage.attr("src", still);
             carImage.attr("data-still", still);
-            carImage.attr("data-animate", animated);
-            carImage.attr("data-state", "still");
+            carImage.attr("data-animated", animated);
+            carImage.attr("data-state", still);
             carImage.addClass("car-image");
-
+            
             carDiv.append(p);
             carDiv.append(carImage);
 
-            $("#images").append(foodDiv);
+            $("#gifs").append(carImages);
         }
     });
 });
 
-// Toggles between still and animate when clicking individual pictures 
-$(document).on("click", ".car-image", function(){
-    let state = $(this).attr("data-state");
+        $(document).on("click", ".car-image", function() {
+            let state = $(this).attr("data-state");
 
-    if (state === "still",){
-         $(this).attr("src", $(this).attr("data-animate"));
-         $(this).attr("data-state", "animate");
-        
-    }
-    else{
-        $(this).attr("src", $(this),attr("data-still"));
-        $(this).attr("data-state", "still");
-    }
-});
-    $("#add-car").on("click", function(event){
-        event.preventDefault();
-        let newFood = $("input").eq(0).val();
+            if (state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+            }
+            
+            else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+            }
+            
+        });
+       $("#add-car").on("click", function(event) {
+           event.preventDefault();
+           let newCar = $("input").eq(0).val();
 
-        if (newcar.length > 2) {
-            cars.push(newCar);
-
-        }
-        populateButtons(cars, "car-button", "#car-buttons");
+         if(newCar.length > 2) {
+             cars.push(newCar);
+         }
+         populateButtons(cars, "car-button", "#food-buttons");  
+       }
     });
-    populateButtons(cars, "car-button", "#car-buttons");
-});
-
+ 
